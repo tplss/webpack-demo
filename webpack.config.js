f6f5a94ff62862088290562085489703c4e44e27
Juho Vepsalainen <bebraw@gmail.com>
2017-02-02 10:50:01 +0200

    Simplify example

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackTemplate = require('html-webpack-template');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const glob = require('glob');
 
 const parts = require('./webpack.parts');
 
@@ -12,50 +9,22 @@ const PATHS = {
   build: path.join(__dirname, 'build'),
 };
 
-const common = merge([
-  {
-    // Entry accepts a path or an object of entries.
-    // We'll be using the latter form given it's
-    // convenient with more complex configurations.
-    //
-    // Entries have to resolve to files! It relies on Node.js
-    // convention by default so if a directory contains *index.js*,
-    // it will resolve to that.
-    entry: {
-      app: PATHS.app,
-    },
-    output: {
-      path: PATHS.build,
-      filename: '[name].js',
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: HtmlWebpackTemplate,
-        title: 'Webpack demo',
-        appMountId: 'app', // Generate #app where to mount
-        mobile: true, // Scale page on mobile
-        inject: false, // html-webpack-template requires this to work
-      }),
-    ],
-  },
-  parts.lintCSS(
-    PATHS.app,
-    {
-      'color-hex-case': 'lower',
-    }
-  ),
-  parts.loadJavaScript(PATHS.app),
-]);
-
-module.exports = function(env) {
+module.exports = function() {
   return merge([
-    common,
     {
+      entry: {
+        app: PATHS.app,
+      },
+      output: {
+        path: PATHS.build,
+        filename: '[name].js',
+      },
       plugins: [
         new webpack.NamedModulesPlugin(),
       ],
     },
     parts.clean(PATHS.build),
+    parts.loadJavaScript(PATHS.app),
     // enable/disable minification to trigger/hide bug
     parts.minifyJavaScript({ useSourceMap: true }),
     parts.generateSourceMaps('cheap-module-source-map'),
