28a7730d00ad00bf55eb16b0e5139ffc90131b0c
Juho Vepsalainen <bebraw@gmail.com>
2017-05-18 10:43:32 +0300

    Add redux-logger to the demo

--- app/index.js
+++ app/index.js
@@ -5,9 +5,14 @@ import './main.css';
 import component from './component';
 import { bake } from './shake';
 
+//import logger from 'redux-logger';
+
+//console.log(logger);
+
 bake();
 
 if (process.env.NODE_ENV === 'development') {
+  require('redux-logger');
   require('./demo');
 }
 
